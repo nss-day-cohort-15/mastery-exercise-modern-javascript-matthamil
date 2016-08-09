17ac7393055222ee500a28e6be230b433038cca3
Matt Hamil <j.matthew.hamil@gmail.com>
2016-08-08 20:40:33 -0500

    Added Jasmine test suite. Testing WIP.

---
+++ jasmine/quiz.spec.js
@@ -0,0 +1,17 @@
+describe('Robot, drone, bipedal, and atv classes exist', function() {
+  it('Robot class exists', function() {
+    expect(Robot).toBeDefined();
+  });
+
+  it('Drone class exists', function() {
+    expect(Drone).toBeDefined();
+  });
+
+  it('Bipedal class exists', function() {
+    expect(Bipedal).toBeDefined();
+  });
+
+  it('ATV class exists', function() {
+    expect(ATV).toBeDefined();
+  });
+});
